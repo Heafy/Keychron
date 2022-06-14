8aff6020d65083a457022849f19c4d89e120ab69
Jorge Martinez <jmtz.2878@gmail.com>
2022-06-14 13:12:42 -0500

    Initial Commit

---
+++ Keychron/KeychronApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychronApp.swift
+//  Keychron
+//
+//  Created by indra on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct KeychronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
